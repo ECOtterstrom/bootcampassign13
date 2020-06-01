e32c1ac02ab20a1480eb323b916fe5129e9ef504
Erin Otterstrom <hapiotter@gmail.com>
2020-06-01 17:04:48 -0600

    created sql db and folder structure

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('plain burger');
+INSERT INTO burgers (burger_name) VALUES ('ketchup burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('mustard burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('the works burger', true);
+INSERT INTO burgers (burger_name) VALUES ('bacon burger');
